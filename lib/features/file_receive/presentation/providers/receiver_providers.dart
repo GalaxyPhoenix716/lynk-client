bab46ed0306cf1565080d6fd97d771d27df62c2a
GalaxyPhoenix <galaxyphoenix716@gmail.com>
2026-07-20 21:54:31 +0530

    implemented receiver provider

--- lib/features/file_receive/presentation/providers/receiver_providers.dart
+++ lib/features/file_receive/presentation/providers/receiver_providers.dart
@@ -1,16 +1,21 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../../../core/network/network_providers.dart';
-import '../../data/datasources/receiver_remote_data_source.dart';
-import '../../data/datasources/receiver_remote_data_source_impl.dart';
-import '../../data/repositories/receiver_repository_impl.dart';
-import '../../domain/repositories/receiver_repository.dart';
+import 'package:client/core/network/network_providers.dart';
+import 'package:client/features/file_receive/data/datasources/receiver_remote_data_source.dart';
+import 'package:client/features/file_receive/data/datasources/receiver_remote_data_source_impl.dart';
+import 'package:client/features/file_receive/data/repositories/receiver_repository_impl.dart';
+import 'package:client/features/file_receive/domain/repositories/receiver_repository.dart';
+import 'package:dio/dio.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-final receiverRemoteDataSourceProvider = Provider<ReceiverRemoteDataSource>((ref) {
-  final dio = ref.watch(dioProvider);
+part 'receiver_providers.g.dart';
+
+@riverpod
+ReceiverRemoteDataSource receiverRemoteDataSource(Ref ref) {
+  final Dio dio = ref.watch(dioProvider);
   return ReceiverRemoteDataSourceImpl(dio: dio);
-});
+}
 
-final receiverRepositoryProvider = Provider<ReceiverRepository>((ref) {
+@riverpod
+ReceiverRepository receiverRepository(Ref ref) {
   final remoteDataSource = ref.watch(receiverRemoteDataSourceProvider);
   return ReceiverRepositoryImpl(remoteDataSource: remoteDataSource);
-});
+}
---
+++ lib/features/file_receive/presentation/providers/receiver_providers.g.dart
@@ -0,0 +1,106 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'receiver_providers.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
+@ProviderFor(receiverRemoteDataSource)
+final receiverRemoteDataSourceProvider = ReceiverRemoteDataSourceProvider._();
+
+final class ReceiverRemoteDataSourceProvider
+    extends
+        $FunctionalProvider<
+          ReceiverRemoteDataSource,
+          ReceiverRemoteDataSource,
+          ReceiverRemoteDataSource
+        >
+    with $Provider<ReceiverRemoteDataSource> {
+  ReceiverRemoteDataSourceProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'receiverRemoteDataSourceProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$receiverRemoteDataSourceHash();
+
+  @$internal
+  @override
+  $ProviderElement<ReceiverRemoteDataSource> $createElement(
+    $ProviderPointer pointer,
+  ) => $ProviderElement(pointer);
+
+  @override
+  ReceiverRemoteDataSource create(Ref ref) {
+    return receiverRemoteDataSource(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(ReceiverRemoteDataSource value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<ReceiverRemoteDataSource>(value),
+    );
+  }
+}
+
+String _$receiverRemoteDataSourceHash() =>
+    r'56a18029eb5167abec14938366f5c62eb5d59e2f';
+
+@ProviderFor(receiverRepository)
+final receiverRepositoryProvider = ReceiverRepositoryProvider._();
+
+final class ReceiverRepositoryProvider
+    extends
+        $FunctionalProvider<
+          ReceiverRepository,
+          ReceiverRepository,
+          ReceiverRepository
+        >
+    with $Provider<ReceiverRepository> {
+  ReceiverRepositoryProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'receiverRepositoryProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$receiverRepositoryHash();
+
+  @$internal
+  @override
+  $ProviderElement<ReceiverRepository> $createElement(
+    $ProviderPointer pointer,
+  ) => $ProviderElement(pointer);
+
+  @override
+  ReceiverRepository create(Ref ref) {
+    return receiverRepository(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(ReceiverRepository value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<ReceiverRepository>(value),
+    );
+  }
+}
+
+String _$receiverRepositoryHash() =>
+    r'3859c00d66b176092e282cee7abd6330d33313cd';
